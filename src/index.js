81bd0a1e459a2a313cd8008d1a332f69860451d0
Theodor Wase <theodor@familjenwase.se>
2022-05-24 14:14:50 +0200

    Header

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ function scrollToTop() {
         top: 0,
         left: 0,
         behavior: 'smooth'
-      });
+    });
 }
 
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
